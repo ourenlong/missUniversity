cf35ec2845882c50226053f361a15e56f199394c
ourenlong <712346172@qq.com>
2018-10-23 22:44:08 +0800

    first

---
+++ src/conn/Conn.java
@@ -0,0 +1,62 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class Conn {
+	//final 不可修改，必须初始化
+  public final static String drivername = "com.mysql.jdbc.Driver";
+  public final static String user = "root";
+  public final static String password = "root";
+  public final static String url = "jdbc:mysql://localhost:3306/gxa";
+  
+  public Connection connection;
+  
+  //构造方法和 类名同名，没有返回值，new 对象的时候调用,初始化基本设置和成员变量
+  //图形界面要打开数据库并且完成查询
+  //1.运行exe
+  //2.填写主机名端口号 用户名 密码后 连接
+  //3.编写查询语句（新开查询编辑器）
+  //4.执行查询得到结果（选中按f8）
+  //5.显示结果
+  public Conn(){
+	  try {
+		Class.forName(drivername);//Class.forName是java反射机制，一个名字可以变成一个类和对象，对应第一步
+		connection = DriverManager.getConnection(url, user, password);//对应的第二步
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  }
+  
+  public void getStudent(){
+	  try {
+		  //对应第3部
+		PreparedStatement prep = connection.prepareStatement("select * from student");
+		ResultSet rs = prep.executeQuery();
+		while(rs.next()){
+			String sno = rs.getString(1);
+			String name = rs.getString(2);
+			String sex = rs.getString(3);
+			float score = rs.getFloat(10);
+			
+			System.out.println("学号："+sno+"姓名 "+name+"性别 "+sex+"分数 "+score);
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+  }
+  
+  public static void main(String[] args) {
+	new Conn().getStudent();
+}
+}
